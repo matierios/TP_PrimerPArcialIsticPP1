7fe842af5a023e793eace28da0c6ef177065b165
matierios <54649634+matierios@users.noreply.github.com>
2019-11-26 20:38:31 -0300

    Validacion Patente OK

--- Rios.Matias/Funciones/HacerIngreso.php
+++ Rios.Matias/Funciones/HacerIngreso.php
@@ -9,10 +9,71 @@ $miobjeto->Patente=$_GET['Patente'];
 date_default_timezone_set("America/Argentina/Buenos_Aires");
 $miobjeto->Horario=mktime();
 
+$alpha6=substr($_GET['Patente'], 0,3);
+$digit6 = substr($_GET['Patente'], 3,3);
+$alpha17=substr($_GET['Patente'], 0,2);
+$digit7=substr($_GET['Patente'], 2,3);
+$alpha27=substr($_GET['Patente'], 5,2);
 
-$query =$BaseDeDatos->prepare("insert into Vehiculos (v_patente,v_horario_ingreso) values ('$miobjeto->Patente','$miobjeto->Horario')");
-$query->execute();			
 
+if (strlen($_GET['Patente']) == 6)
+	{
+			if (ctype_alpha($alpha6)==true)
+				{
+					if (ctype_digit($digit6)==true) 
+						{
+							$query =$BaseDeDatos->prepare("insert into Vehiculos (v_patente,v_horario_ingreso) values ('$miobjeto->Patente','$miobjeto->Horario')");
+							$query->execute();			
 
-header("Location: ..\Paginas\VehiculoOK.php");
+							header("Location: ..\Paginas\VehiculoOK.php");
+						}
+					else
+						{
+							header("Location: ..\Paginas\VehiculoNoOK.php");
+
+						}	
+				}		 
+			else
+				{
+					header("Location: ..\Paginas\VehiculoNoOK.php");
+				}		
+		
+	}
+else
+	{
+		if (strlen($_GET['Patente']) == 7)
+		{
+			 if (ctype_alpha($alpha17)==true)
+			 	{
+			 		if (ctype_digit($digit7)==true) 
+			 			{
+			 				if (ctype_alpha($alpha27)==true)
+			 					{
+			 						$query =$BaseDeDatos->prepare("insert into Vehiculos (v_patente,v_horario_ingreso) values ('$miobjeto->Patente','$miobjeto->Horario')");
+									$query->execute();			
+
+									header("Location: ..\Paginas\VehiculoOK.php");
+			 					}
+			 				else
+			 					{
+			 						header("Location: ..\Paginas\VehiculoNoOK.php");			
+			 					}	
+			 			}
+			 		else
+			 			{
+			 				header("Location: ..\Paginas\VehiculoNoOK.php");
+			 			}	
+			 	}
+			 else
+			 	{
+			 		header("Location: ..\Paginas\VehiculoNoOK.php");
+			 	}	
+		}
+	else
+		{
+			 header("Location: ..\Paginas\VehiculoNoOK.php");
+		}
+
+	}
+
 ?>
